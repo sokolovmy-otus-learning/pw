d6bca1cba0e246b1320f964df40d9469aa2a2174
Maxim Sokolov <holse@yandex.ru>
2025-05-18 23:26:49 +0400

    19

--- k8s/monitoring/1.sh
+++ k8s/monitoring/1.sh
@@ -1,45 +1,18 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
 
 helm repo update
 
-# metric server
-
-helm upgrade --install metrics-server metrics-server/metrics-server \
-  --namespace kube-system \
-  -f metrics-server-values.yaml
 
 
 
+helm upgrade --install loki grafana/loki-stack -n monitoring --create-namespace --set loki.isDefault=false
+
+helm upgrade --install prom prometheus-community/kube-prometheus-stack \
+  -n monitoring --set alertmanager.enabled=false --set grafana.sidecar.datasources.alertmanager=false
 
 
-# helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack -n monitoring --create-namespace
-
-helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
-  -n monitoring --create-namespace \
-  -f grafana-values.yaml
-
-
-helm install kube-state-metrics prometheus-community/kube-state-metrics -n monitoring
-
-
-
-# kubectl port-forward svc/kube-prometheus-stack-grafana 3000:80 -n monitoring
-# Откройте http://localhost:3000 (по умолчанию логин: admin, пароль: prom-operator).
-
-
-
-
-# loki
-
-
-helm install loki grafana/loki-stack -n monitoring \
-  --create-namespace \
-  --set grafana.enabled=false \
-  --set promtail.enabled=true \
-  --set promtail.config.clients[0].url=http://loki.monitoring.svc:3100/loki/api/v1/push
-
-
-
-kubectl apply -f loki-dashboards.yaml
+# helm upgrade --install grafana grafana/grafana -n monitoring --set sidecar.datasources.enabled=true \
+#   --set sidecar.dashboards.enabled=true \
+#   --set persistence.enabled=true \
+#   --set persistence.size=1Gi
